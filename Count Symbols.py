515109ced77a3910acd2ea95239786ecd5300cf9
Tsvetkova21 <119804280+Tsvetkova21@users.noreply.github.com>
2023-02-04 18:24:24 +0200

    Delete Count Symbols.py

--- Count Symbols.py
+++
@@ -1,8 +0,0 @@
-occurrences = {}
-for l in input():
-    if l not in occurrences:
-        occurrences[l] =0
-    occurrences[l]+=1
-
-for letter, times in sorted(occurrences.items()):
-    print(f" {letter}: {times} time/s")
