5a297626bc8ba413484e55a0f97f890aa9b9dc7e
Tsvetkova21 <119804280+Tsvetkova21@users.noreply.github.com>
2023-02-04 18:24:17 +0200

    Delete Count Same Values_v2.py

--- Count Same Values_v2.py
+++
@@ -1,5 +0,0 @@
-numbers = tuple(map(float, input().split(" ")))
-nums_and_occurances = {}
-for items in numbers:
-    nums_and_occurances[items]=numbers.count(items)
-[print(f"{key} - {value} times") for key, value in nums_and_occurances.items()]
